279ea7db32aea18b58a4ba1debd3fa474d6bfff7
Rob Harrison <robwithhair@gmail.com>
2018-10-10 11:31:28 +0100

    Updates

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "Anchorage",
     products: [
         .library(name: "Anchorage", targets: ["Anchorage"]),
+        .library(name: "Anchorage-CLI", targets: ["Anchorage-CLI"]),
         .executable(name: "anchor", targets: ["Anchor"]),
     ],
     dependencies: [
@@ -19,8 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Anchor",
-            dependencies: ["Anchorage", "Utility"]
+            dependencies: ["Anchorage-CLI", "Utility"]
         ),
+        .target(
+            name: "Anchorage-CLI",
+            dependencies: ["Anchorage"]),
         .target(
             name: "Anchorage",
             dependencies: []),
---
+++ Sources/Anchorage-CLI/test.swift
@@ -0,0 +1,12 @@
+//
+//  test.swift
+//  Anchor
+//
+//  Created by Robert Harrison on 10/10/2018.
+//
+
+import Foundation
+
+public func test() -> String {
+    return "Hello, World"
+}
