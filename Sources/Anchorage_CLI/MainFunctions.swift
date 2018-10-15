cc46458e1e9fb14cfa8d5038d79272add4c1ec7a
Rob Harrison <robwithhair@gmail.com>
2018-10-15 21:36:35 +0100

    Working version before bed

--- Sources/Anchorage_CLI/MainFunctions.swift
+++ Sources/Anchorage_CLI/MainFunctions.swift
@@ -11,44 +11,30 @@ import Utility
 import Basic
 
 public func main(withArguments arguments: [String], commandName: String, overview: String) {
+    
+    let argumentParser = ArgumentParser(commandName: commandName, usage: "<COMMAND> [options]", overview: overview, seeAlso: nil)
+    
+    let commands = rootCommands(for: argumentParser)
+    
     do {
-        let argumentParser = ArgumentParser(commandName: commandName, usage: "<COMMAND> [options]", overview: overview, seeAlso: nil)
-        
-        let clusterParser = argumentParser.add(subparser: "cluster", overview: NSLocalizedString("Create, destroy and manage clusters.", comment: "Cluster command overview"))
-        let clusterCreateParser = clusterParser.add(subparser: "create", overview: NSLocalizedString("Create new clusters.", comment: "Create command overview"))
-        let clusterRemoveParser = clusterParser.add(subparser: "rm", overview: NSLocalizedString("Delete the specified cluster", comment: "rm command overview"))
-        let clusterListParser = clusterParser.add(subparser: "ls", overview: NSLocalizedString("List all managed clusters.", comment: "List command overview"))
-        
-        let clusterCreateNames = clusterCreateParser.add(positional: "name", kind: [String].self, optional: false, strategy: .oneByOne, usage: "Name argument usage", completion: .none)
-        
-        let clusterRemoveNames = clusterRemoveParser.add(positional: "name", kind: [String].self, optional: false, strategy: .oneByOne, usage: "Name argument usage", completion: .none)
-        
-        // add subcommands
         
         let result = try argumentParser.parse(prepare(arguments: arguments))
         
-        if let subparser = result.subparser(argumentParser) {
-            print("Got here 1")
-            switch subparser {
-            case "cluster":
-                print("Got this far")
-                if let subparser = result.subparser(clusterParser) {
-                    if subparser == "ls" {
-                        print(
-                            try Cluster.list(using: FileManager.default).joined(separator: "\t")
-                        )
-                    }
-                    print(subparser)
-                    return
-                }
-            default:
-                break
-            }
+        guard let subparser = result.subparser(argumentParser) else {
+            argumentParser.printUsage(on: stderrStream)
+            exit(1)
         }
         
-        argumentParser.printUsage(on: stderrStream)
-        exit(1)
+        let toRun = commands.compactMap { $0.name == subparser ? $0 : nil }
         
+        if toRun.isEmpty {
+            argumentParser.printUsage(on: stderrStream)
+            exit(1)
+        } else {
+            try toRun.forEach { (command) in
+                try command.run(result)
+            }
+        }
     } catch let error as ArgumentParserError {
         handle(error: error)
     } catch let error {
--- Tests/Anchorage_CLITests/Anchorage_CLITests.swift
+++ Tests/Anchorage_CLITests/Anchorage_CLITests.swift
@@ -3,34 +3,53 @@ import class Foundation.Bundle
 @testable import Anchorage_CLI
 
 final class Anchorage_CLITests: XCTestCase {
-
-
-    func testExample() throws {
+    
+    func run(commands: [String]) throws -> (String, String) {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
+        
         let fooBinary = productsDirectory.appendingPathComponent("anchor")
-
+        
         let process = Process()
-        process.arguments = ["cluster", "ls"]
-        process.executableURL = fooBinary
-
+        process.arguments = commands
+        if #available(OSX 10.13, *) {
+            process.executableURL = fooBinary
+        } else {
+            process.launchPath = fooBinary.path
+        }
+        
         let pipe = Pipe()
+        let errorPipe = Pipe()
         process.standardOutput = pipe
-
-        try process.run()
+        process.standardError = errorPipe
+        
+        if #available(OSX 10.13, *) {
+            try process.run()
+        } else {
+            process.launch()
+        }
         process.waitUntilExit()
-
+        
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)
-        XCTAssertEqual( output!, "foo")
+        let errorOutput = String(data: errorData, encoding: .utf8)
+//        XCTAssertEqual(errorOutput, "")
+        return (output!, errorOutput!)
         //XCTAssertTrue( output!.contains("command is used to manage a docker swarm cluster"))
+    }
+    
+    func testEmptyList() throws {
+        let (output, error) = try run(commands: ["cluster", "ls"])
+        XCTAssertEqual(output, "")
+        XCTAssertEqual(error, "")
+    }
+    
+    func testEmptyCreate() throws {
+        let (output, error) = try run(commands:["cluster", "create"])
+        XCTAssertEqual(output, "")
+        XCTAssertTrue(error.contains("expected arguments: names"), "Expected to recieve expected arguments string")
     }
 
     /// Returns path to the built products directory.
@@ -46,6 +65,6 @@ final class Anchorage_CLITests: XCTestCase {
     }
 
     static var allTests = [
-      ("testExample", testExample),
+      ("testEmptyList", testEmptyList),
     ]
 }
